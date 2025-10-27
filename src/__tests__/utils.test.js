93117132f2d987ef711f86b2e70f42ec134df838
yg-bit <jamesisaiah164@gmail.com>
2025-10-27 08:07:23 +0300

    added

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,9 +1,11 @@
+// src/__tests__/utils.test.js
 import { currentAgeForBirthYear } from "../utils";
 
 describe("currentAgeForBirthYear", () => {
   it("returns the age of a person based on the year of birth", () => {
     const birthYear = 1984;
+    const expectedAge = new Date().getFullYear() - birthYear;
     const ageOfPerson = currentAgeForBirthYear(birthYear);
-    expect(ageOfPerson).toBe(38);
+    expect(ageOfPerson).toBe(expectedAge);
   });
 });
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,6 @@
 // Your code here
+// src/utils.js
+export function currentAgeForBirthYear(birthYear) {
+  const currentYear = new Date().getFullYear();
+  return currentYear - birthYear;
+}
